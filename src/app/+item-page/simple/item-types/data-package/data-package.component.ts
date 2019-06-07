ee68cd0406cf561def1045853a5a7b270d83285f
Art Lowel <art.lowel@atmire.com>
2019-06-07 17:30:20 +0200

    configure datapackage pages

--- src/app/+item-page/simple/item-types/data-package/data-package.component.ts
+++ src/app/+item-page/simple/item-types/data-package/data-package.component.ts
@@ -27,17 +27,7 @@ export class DataPackageComponent extends ItemComponent implements OnInit {
   /**
    * The projects related to this publication
    */
-  projects$: Observable<Item[]>;
-
-  /**
-   * The organisation units related to this publication
-   */
-  orgUnits$: Observable<Item[]>;
-
-  /**
-   * The journal issues related to this publication
-   */
-  journalIssues$: Observable<Item[]>;
+  dataFiles$: Observable<Item[]>;
 
   constructor(
     @Inject(ITEM) public item: Item,
@@ -53,18 +43,8 @@ export class DataPackageComponent extends ItemComponent implements OnInit {
 
       this.authors$ = this.buildRepresentations('Person', 'dc.contributor.author', this.ids);
 
-      this.projects$ = this.resolvedRelsAndTypes$.pipe(
-        filterRelationsByTypeLabel('isProjectOfPublication'),
-        relationsToItems(this.item.id, this.ids)
-      );
-
-      this.orgUnits$ = this.resolvedRelsAndTypes$.pipe(
-        filterRelationsByTypeLabel('isOrgUnitOfPublication'),
-        relationsToItems(this.item.id, this.ids)
-      );
-
-      this.journalIssues$ = this.resolvedRelsAndTypes$.pipe(
-        filterRelationsByTypeLabel('isJournalIssueOfPublication'),
+      this.dataFiles$ = this.resolvedRelsAndTypes$.pipe(
+        filterRelationsByTypeLabel('isDataFileOfDataPackage'),
         relationsToItems(this.item.id, this.ids)
       );
 
