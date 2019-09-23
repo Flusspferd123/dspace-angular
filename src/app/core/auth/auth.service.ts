6e8655595d49ed22c0674043ab03401eae5cc0c3
Julius Gruber <julius.gruber@tuwien.ac.at>
2019-09-23 17:45:38 +0200

    Router history mock up

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -382,7 +382,8 @@ export class AuthService {
               // For standalone login pages, use the previous route.
               redirUrl = history[history.length - 2] || '';
             } else {
-              redirUrl = history[history.length - 1] || '';
+              // redirUrl = history[history.length - 1] || '';
+              redirectUrl = '/home'
             }
             this.navigateToRedirectUrl(redirUrl);
           });
