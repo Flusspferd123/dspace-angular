707600bda4992cc9f2786dc56891b5ad47679371
Julius Gruber <julius.gruber@tuwien.ac.at>
2019-09-23 14:46:43 +0200

    Added Subscription to loginSucess

--- src/app/+login-page/shibboleth/shibboleth.component.ts
+++ src/app/+login-page/shibboleth/shibboleth.component.ts
@@ -4,6 +4,8 @@ import { select, Store } from '@ngrx/store';
 import {CoreState} from '../../core/core.reducers';
 import {Observable, of} from 'rxjs';
 import { isAuthenticated, isAuthenticationLoading } from '../../core/auth/selectors';
+import { filter, takeWhile } from 'rxjs/operators';
+import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'ds-shibboleth-page',
@@ -19,6 +21,12 @@ export class ShibbolethComponent implements OnInit {
   public loading: Observable<boolean>;
   public isAuthenticated: Observable<boolean>;
 
+  /**
+   * Component state.
+   * @type {boolean}
+   */
+  private alive = true;
+
   ngOnInit() {
     // set isAuthenticated
     this.isAuthenticated = this.store.pipe(select(isAuthenticated));
@@ -28,8 +36,18 @@ export class ShibbolethComponent implements OnInit {
     this.loading = this.store.pipe(select(isAuthenticationLoading));
 
     this.store.dispatch(new GetJWTafterShibbLoginAction());
+
+    // subscribe to success
+    this.store.pipe(
+      select(isAuthenticated),
+      takeWhile(() => this.alive),
+      filter((authenticated) => authenticated))
+      .subscribe(() => {
+          this.authService.redirectAfterLoginSuccess(true);
+        }
+      );
   }
 
-  constructor( private store: Store<CoreState>,) { }
+  constructor( private store: Store<CoreState>, private authService: AuthService) { }
 
 }
