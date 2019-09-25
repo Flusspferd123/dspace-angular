1b8329dd89c1fc246ab2aba33b683342872b2c30
Julius Gruber <julius.gruber@tuwien.ac.at>
2019-09-25 18:21:10 +0200

    Added OA  widgets to licence section

--- src/app/submission/sections/license/section-license.model.ts
+++ src/app/submission/sections/license/section-license.model.ts
@@ -1,7 +1,15 @@
+import { DynamicRadioGroupModel } from '@ng-dynamic-forms/core';
 
 export const SECTION_LICENSE_FORM_LAYOUT = {
 
   granted: {
+    element: {
+      container: 'custom-control custom-checkbox pl-1',
+      control: 'custom-control-input',
+      label: 'custom-control-label pt-1'
+    }
+  },
+  openAccess: {
     element: {
       container: 'custom-control custom-checkbox pl-1',
       control: 'custom-control-input',
@@ -24,5 +32,36 @@ export const SECTION_LICENSE_FORM_MODEL = [
       notgranted: 'You must accept the license'
     },
     type: 'CHECKBOX',
-  }
+  },
+  {
+    id: 'openAccess',
+    label: 'I confirm that this publication is published under an Open Access licence in the TU\'s reposiTUm',
+    required: false,
+    value: false,
+    validators: {
+      required: null
+    },
+    errorMessages: {
+      required: 'You must accept the license',
+      notgranted: 'You must accept the license'
+    },
+    type: 'CHECKBOX',
+  },
+  new DynamicRadioGroupModel<string>({
+
+    id: 'OpenAccessPayment',
+    label: 'Payment for Open Access is executed by',
+    options: [
+      {
+        label: 'TU Biblothek',
+        value: 'TU_Bibliothek',
+      },
+      {
+        label: 'Insitut / FWF',
+        value: 'Institut/FWF'
+      },
+
+    ],
+    // value: "option-3"
+  }),
 ];
